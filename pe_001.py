033a5c505daeb14433b062e809b01beebf935ee6
Kris Bucyk <krisbucyk@gmail.com>
2019-12-04 21:35:02 -0500

    Generalize function, clean up code

--- pe_001.py
+++ pe_001.py
@@ -1,16 +1,20 @@
 """
-PE Problem 1
-jar-jar-binks
-08.30.2017
+PE Problem #001
+Title: Multiples of 3 and 5
+Written by Kris Bucyk (github.com/RiceKrisBs)
 """
 
-def trial():
-    sum = 0
-    for i in range(1, 1000):
-        if i % 3 == 0 or i % 5 == 0:
-            sum = sum + i
-        else:
-            sum = sum
-    print(sum)
+def sum_multiples(x, y, z):
+    # find the sum of all multiples of x or y below z
+    if z < min(x, y):
+        return 0
 
-trial()
+    total = 0
+    for i in range(min(x, y), z):
+        if i % x == 0 or i % y == 0:
+            total += i
+    return total
+
+
+if __name__ == '__main__':
+    print(sum_multiples(3, 5, 1000))
