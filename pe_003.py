6d87a11af87dca30478217fe84ab8a4119986f9a
Kris Bucyk <krisbucyk@gmail.com>
2019-12-06 12:34:16 -0500

    Add TODO comment for refactor ideas

--- pe_003.py
+++ pe_003.py
@@ -4,20 +4,29 @@ Title: Largest prime factor
 Written by Kris Bucyk (github.com/RiceKrisBs)
 """
 
-from math import sqrt, ceil
+import math
 
+
+# TODO: refactor is_prime so that instead of checking all numbers up to n,
+# it only checks up to math.ceil(math.sqrt(n))
+# TODO: refactor is_prime so that instead of checking all numbers up to
+# sqrt(n), it checks nubmers that are of the form 6i+-1 that are <= sqrt(n)
+# TODO: refactor find_largest_prime_factor so that it only checks values that
+# are of the form 6i+-1.
 def is_prime(n):
-    for i in range(2,n):
+    for i in range(2, n):
         if n % i == 0:
             return False
-    return n
+    return True
 
 def find_largest_prime_factor(number):
-    limit = ceil(sqrt(number)) + 1
+    limit = math.ceil(math.sqrt(number)) + 1
     largest_prime = number
-    for k in range (2,limit):
+    for k in range (2, limit):
         if number % k == 0 and is_prime(k):
-            largest_prime = is_prime(k)
+            largest_prime = k
     return largest_prime
 
-print(find_largest_prime_factor(600851475143))
+
+if __name__ == '__main__':
+    print(find_largest_prime_factor(600851475143))
