106b6691c83e7dffd0b22b7f0532435c2c912fd8
Kris Bucyk <krisbucyk@gmail.com>
2019-12-07 13:53:35 -0500

    Refactor/generalize functions

--- pe_005.py
+++ pe_005.py
@@ -1,60 +1,30 @@
 """
-PE Problem 5
-jar-jar-binks
-09.14.2017
+PE Problem #005
+Title: Smallest multiple
+Written by Kris Bucyk (github.com/RiceKrisBs)
 """
 
-# def multiple():
-#     counter = 20
-#     while True:
-#         a = counter % 11
-#         b = counter % 12
-#         c = counter % 13
-#         d = counter % 14
-#         e = counter % 15
-#         f = counter % 16
-#         g = counter % 17
-#         h = counter % 18
-#         i = counter % 19
-#         j = counter % 20
-#         sum = a+b+c+d+e+f+g+h+i+j
-#         #print(counter)
-#         if sum == 0:
-#             return counter
-#         counter += 1
+import pe_003
 
-#print(multiple())
-#print(1*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20)
-# 2  = 2^1
-# 3  =     3^1
-# 4  = 2^2
-# 5  =         5^1
-# 6  = 2^1 3^1
-# 7  =             7^1
-# 8  = 2^3
-# 9  =     3^2
-# 10 = 2^1     5^1
-# 11 =                 11^1
-# 12 = 2^2 3^1
-# 13 =                      13^1
-# 14 = 2^1         7^1
-# 15 =     3^1 5^1
-# 16 = 2^4
-# 17 =                           17^1
-# 18 = 2^1 3^2
-# 19 =                                19^1
-# 20 = 2^2     5^1
+def find_product_of_primes(k):
+    # finds product of all primes from 2 to k, inclusive
+    prod = 1
+    for i in range(2, k + 1):
+        if pe_003.is_prime(i):
+            prod *= i
+    return prod
 
-test = (2**4) * (3**2) * 5 * 7 * 11 * 13 * 17 * 19
-a = test % 11
-b = test % 12
-c = test % 13
-d = test % 14
-e = test % 15
-f = test % 16
-g = test % 17
-h = test % 18
-i = test % 19
-j = test % 20
-sum = a+b+c+d+e+f+g+h+i+j
-print(test)
+def find_smallest_multiple(k):
+    # finds smallest positive number evenly divisible by 1 through k
+    n1 = find_product_of_primes(k)
+    n2 = 1
+    divisors = [x for x in range(2, k + 1)]
+    while True:
+        test = n1 * n2
+        if sum([test % i for i in divisors]) == 0:
+            return test
+        n2 += 1
+
+
+if __name__ == '__main__':
+    print(find_smallest_multiple(20))
