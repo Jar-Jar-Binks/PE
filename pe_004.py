6cf16e3bce4f4348a21b0e979ea74da15b0edd32
Kris Bucyk <krisbucyk@gmail.com>
2019-12-07 07:24:27 -0500

    Refactor and generalize for product of two n-digit numbers

--- pe_004.py
+++ pe_004.py
@@ -1,27 +1,24 @@
 """
-PE Problem 4
-jar-jar-binks
-09.21.2017
+PE Problem #004
+Title: Largest palindrome product
+Written by Kris Bucyk (github.com/RiceKrisBs)
 """
 
-def is_palindrome(word):
-    txt = str(word)
-    backwards = txt[::-1]
-    if txt == backwards:
-        return True
-    return False
-
-def find_palindrome():
-    n1 = 100
+def largest_palindrome(n):
+    # finds largest palindrome made from the product of two n-digit numbers
+    start = 10 ** (n - 1)
     largest_palindrome = 0
-    while n1 <=999:
+
+    # start testing pairs of n-digit numbers where n1 <= n2.
+    for n1 in range(start, 10 ** n):
         n2 = n1
-        while n2 <=999:
-            prod = n1 * n2
-            if is_palindrome(prod) and prod > largest_palindrome:
-                largest_palindrome = prod
+        while n2 < 10 ** n:
+            temp_prod = n1 * n2
+            if str(temp_prod) == str(temp_prod)[::-1] and temp_prod > largest_palindrome:
+                largest_palindrome = temp_prod
             n2 += 1
-        n1 += 1
     return largest_palindrome
 
-print(find_palindrome())
+
+if __name__ == '__main__':
+    print(largest_palindrome(3))
