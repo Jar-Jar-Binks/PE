599783c0fc57f1ab5bfe4598658d7d3b61b33a83
Kris Bucyk <krisbucyk@gmail.com>
2019-12-14 11:59:05 -0500

    Generalize solution

--- pe_006.py
+++ pe_006.py
@@ -1,24 +1,15 @@
 """
-PE Problem 6
-jar-jar-binks
-09.12.2017
+PE Problem #006
+Title: Sum square difference
+Written by Kris Bucyk (github.com/RiceKrisBs)
 """
 
-index = 100
+def sum_square(n):
+    return sum([n**2 for n in range(1, n + 1)])
 
-def sum_square():
-    sum_square_sum = 0
-    for i in range(1,index+1):
-        sum_square_sum = sum_square_sum + i **2
-    return sum_square_sum
+def square_sum(n):
+    return sum([n for n in range(1, n + 1)]) ** 2
 
-def square_sum():
-    sum = 0
-    for i in range(1,index+1):
-        sum = sum + i
-    return sum ** 2
 
-def difference():
-    return sum_square() - square_sum()
-
-print(difference())
+if __name__ == '__main__':
+    print(abs(sum_square(100) - square_sum(100)))
